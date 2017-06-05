fe77974f65eeded14d7d99b1492d6878e5872e01
Alex Couture-Beil <alex@mofo.ca>
2017-06-05 15:47:33 -0700

    renamed to acbutils

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import re
 from setuptools import find_packages, setup
 
 setup(
-    name='pyutils',
+    name='acbutils',
     version='0.0.1',
     description='misc python utilties',
     long_description='misc python utilties',
-    url='https://github.com/alexcb/pyutils',
+    url='https://github.com/alexcb/acbutils',
     author='Alex Couture-Beil and contributors',
     license='BSD',
     packages=find_packages(),
