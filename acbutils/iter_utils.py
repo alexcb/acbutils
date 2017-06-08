52890616f7a1c1b14a6ff5edbe72a1dced688935
Alex Couture-Beil <alex@mofo.ca>
2017-06-08 11:37:29 -0700

    iterate by groups

---
+++ acbutils/iter_utils.py
@@ -0,0 +1,23 @@
+import itertools
+
+def grouper(n, iterable):
+    '''grouper(3, "abcd") -> [['a', 'b', 'c'], ['d']]'''
+    i = iter(iterable)
+    running = True
+    while running:
+        args = []
+        for _ in xrange(n):
+            try:
+                args.append(next(i))
+            except StopIteration:
+                running = False
+        if args:
+            yield args
+
+if __name__ == '__main__':
+    def test_grouper():
+        for x in grouper(3, xrange(10)):
+            print x
+        for x in grouper(3, "abc"):
+            print x
+    test_grouper()
