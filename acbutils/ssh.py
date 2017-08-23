e54168cc86d0972376553efc6f2c32c584d1ff78
Alex Couture-Beil <alex@mofo.ca>
2017-08-23 16:39:52 -0700

    added sudo option

--- acbutils/ssh.py
+++ acbutils/ssh.py
@@ -33,23 +33,25 @@ def build_remote_script(script, remotelib=None):
     ''' % data)
 
 
-def run_script_over_ssh(script, host):
-    p = subprocess.Popen(['ssh', host, 'python', '-'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+def run_script_over_ssh(script, host, sudo=False):
+    sudo_cmd = ['sudo'] if sudo else []
+    cmd = ['ssh', host] + sudo_cmd + ['python', '-']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
     out = p.communicate(input=script)[0]
     return p.returncode, out.decode()
 
 
-def run_script_over_ssh_parallel(script, hosts, max_conn=4):
+def run_script_over_ssh_parallel(script, hosts, sudo=False, max_conn=4):
     def helper(args):
         script, host = args
-        return run_script_over_ssh(script, host)
+        return run_script_over_ssh(script, host, sudo=sudo)
     p = multiprocessing.dummy.Pool(max_conn)
     return p.map(helper, [(script, x) for x in hosts])
 
-def run_scripts_over_ssh_parallel(script, hosts, max_conn=4):
+def run_scripts_over_ssh_parallel(script, hosts, sudo=False, max_conn=4):
     assert len(script) == len(hosts)
     def helper(args):
         script, host = args
-        return run_script_over_ssh(script, host)
+        return run_script_over_ssh(script, host, sudo=sudo)
     p = multiprocessing.dummy.Pool(max_conn)
     return p.map(helper, zip(script, hosts))
