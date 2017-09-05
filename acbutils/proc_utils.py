c3318918d105ee7e661a989a0e53adb269435f4b
Alex Couture-Beil <alex@mofo.ca>
2017-09-05 10:53:20 -0700

    communicate stream tweaks

--- acbutils/proc_utils.py
+++ acbutils/proc_utils.py
@@ -4,15 +4,15 @@ import time
 from Queue import Queue, Empty
 
 def _readerthread(fh, piperef, q):
-    while True:
-        x = fh.readline()
-        if x == '':
-            break
-        q.put((piperef, x.strip('\n')))
-
+    if fh:
+        l = fh.readline()
+        while l:
+            q.put((piperef, l.strip('\n')))
+            l = fh.readline()
+        fh.close()
     q.put((piperef, None))
 
-def communicate_stream(p):
+def communicate_stream(p, input=None):
     q = Queue()
     stdout_thread = threading.Thread(target=_readerthread,
                                      args=(p.stdout, 1, q))
@@ -24,6 +24,30 @@ def communicate_stream(p):
     stderr_thread.setDaemon(True)
     stderr_thread.start()
 
+    if input:
+        try:
+            p.stdin.write(input)
+        except BrokenPipeError:
+            pass  # communicate() must ignore broken pipe errors.
+        except OSError as exc:
+            if exc.errno == errno.EINVAL:
+                # bpo-19612, bpo-30418: On Windows, stdin.write() fails
+                # with EINVAL if the child process exited or if the child
+                # process is still running but closed the pipe.
+                pass
+            else:
+                raise
+
+    try:
+        p.stdin.close()
+    except BrokenPipeError:
+        pass  # communicate() must ignore broken pipe errors.
+    except OSError as exc:
+        if exc.errno == errno.EINVAL:
+            pass
+        else:
+            raise
+
     num_none = 0
     while 1:
         piperef, line = q.get()
@@ -32,5 +56,14 @@ def communicate_stream(p):
             if num_none >= 2:
                 break
         else:
-            yield line
+            yield piperef, line
 
+    stdout_thread.join()
+    stderr_thread.join()
+
+
+if __name__ == '__main__':
+    cmd = ['ls']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for piperef, line in communicate_stream(p):
+        print piperef, line
