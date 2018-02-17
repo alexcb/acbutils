a40f56b3aeadf547a5186d916d292e21b374ad45
Alex Couture-Beil <alex@mofo.ca>
2018-02-17 13:16:50 -0800

    fixed justify bug in tabulate

--- acbutils/tabulate.py
+++ acbutils/tabulate.py
@@ -63,10 +63,33 @@ def tabulate(*args, **kwargs):
             buf.append(csep)
         return ''.join(buf)
 
-    justify = None
-    rows_groups = []
     for rows in args:
         ensure_same_col_types(rows)
+
+    first_row = args[0][0]
+    expected_len = len(first_row)
+    for rows in args:
+        for row in rows:
+            assert(len(row) == expected_len)
+
+    justify = kwargs.get('justify')
+    if justify is None:
+        #import pdb; pdb.set_trace()
+        justify = ['l'] * len(args[0][0])
+        for rows in args:
+            for row in rows:
+                for i, col in enumerate(row):
+                    if isinstance(col, float):
+                        print 'changing justify type'
+                        print justify, i
+                        justify[i] = 'r'
+    else:
+        if len(justify) != expected_len:
+            raise ValueError("justify string incorrect length")
+        justify = list(justify)
+
+    rows_groups = []
+    for rows in args:
         for row in rows:
             if justify is None:
                 justify = ['l'] * len(row)
@@ -117,40 +140,6 @@ def tabulate(*args, **kwargs):
 
     return '\n'.join(buf)
 
-
-
-
-    rows = [[[to_str(c) for c in row] for row in group_of_rows] for group_of_rows in rows]
-
-    assert len(set(len(y) for x in rows for y in x)) == 1, "not all rows have the same number of elements"
-    lengths = [[len(str(xx)) for xx in y] for x in rows for y in x]
-    padding = [max(x) for x in zip(*lengths)]
-    width = sum(padding) + len(padding) * 3 + 1
-    justify = kwargs.get('justify', 'l' * len(padding))
-    assert len(justify) == len(padding), 'padding doesnt match length of columns'
-    ljust = lambda s, pad: s.ljust(pad)
-    rjust = lambda s, pad: s.rjust(pad)
-
-    def f(s, j, n):
-        if s is None:
-            s = ''
-        if isinstance(s, float):
-            s = '%.3f' % s
-        s = str(s)
-        if j == 'l':
-            return s.ljust(n)
-        if j == 'r':
-            return s.rjust(n)
-
-
-    buf = []
-    for i, rr in enumerate(rows):
-        buf.append('-' * width)
-        for r in rr:
-            s = '|'.join(' %s ' % f(x, j, n) for x, j, n in zip(r, justify, padding))
-            buf.append('|%s|' % s)
-    buf.append('-' * width)
-    return '\n'.join(buf)
 
 if __name__ == '__main__':
     headers = [
