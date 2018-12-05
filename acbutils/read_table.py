129d5ddf6a5bd388124ff5af83cabc2e11ec1a11
Alex Couture-Beil <alex@mofo.ca>
2018-12-04 21:14:05 -0800

    make read_table work with right-aligned numerical colums as produced by tablulate with style=min

--- acbutils/read_table.py
+++ acbutils/read_table.py
@@ -17,25 +17,55 @@ def get_word_pos(s):
 
     return pos
 
+def get_col_pos(lines):
+    blanks = []
+    for l in lines:
+        assert '\t' not in l
+        if len(l) > len(blanks):
+            n = len(l) - len(blanks)
+            blanks = blanks + [True]*n
+        for i, c in enumerate(l):
+            if c != ' ':
+                blanks[i] = False
+
+    pos = []
+    looking = True
+    for i, c in enumerate(blanks):
+        if looking:
+            if c == False:
+                pos.append(i)
+                looking = False
+            continue
+        else:
+            if c == True:
+                looking = True
+
+    headers = get_words_at_pos(lines[0], pos)
+
+
+    pos2 = []
+    for s, i in zip(headers, pos):
+        if s:
+            pos2.append(i)
+
+    return pos2
+
 def get_words_at_pos(s, pos):
     start = pos
     end = [x-1 for x in pos[1:]]
     end.append(None)
     return [s[i:j].strip() for i, j in zip(start,end)]
 
-
 def read_table(s, col_parsers={}):
     lines = s.split('\n')
     lines = [x for x in lines if x.strip()]
-    headers = get_word_pos(lines[0])
+    col_pos = get_col_pos(lines)
+    headers = get_words_at_pos(lines[0], col_pos)
     lines = lines[1:]
-
-    colnames = [x[0] for x in headers]
-    colpos =   [x[1] for x in headers]
 
     rows = []
     for l in lines:
-        rows.append(dict(list(zip(colnames, get_words_at_pos(l, colpos)))))
+        rows.append(dict(list(zip(headers, get_words_at_pos(l, col_pos)))))
 
     for r in rows:
         for k in r:
