a22dfa351c8ea81a19ef668ae14b7169b49a80e2
Alex Couture-Beil <alex@mofo.ca>
2021-10-12 18:34:35 -0700

    default prompt option

--- acbutils/prompt.py
+++ acbutils/prompt.py
@@ -1,8 +1,21 @@
-def prompt(msg):
-    while True:
-        answer = eval(input("%s [yes/no]" % msg))
-        if answer.lower() in ("yes", "y"):
-            return True
-        if answer.lower() in ("no", "n"):
-            return False
+def prompt(msg, default=None):
+    yes = ("yes", "y")
+    no = ("no", "n")
 
+    if default is None:
+        msg += " [yes/no]"
+    elif default.lower() in yes:
+        msg += " [Yes/no]"
+    elif default.lower() in  no:
+        msg += " [yes/No]"
+    else:
+        raise ValueError(default)
+
+    while True:
+        answer = input(msg)
+        if answer == '' and default:
+            answer = default
+        if answer.lower() in yes:
+            return True
+        if answer.lower() in no:
+            return False
